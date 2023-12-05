2b45c7806f9d2a3f35d572fbe925bd54723a6f9e
hannahtran8 tran <hannah.tran@sciacademy.org>
2023-12-05 23:26:49 +0000

    refactor to create 100 circles using loops

--- projects/circularity/js/init.js
+++ projects/circularity/js/init.js
@@ -79,7 +79,9 @@ var init = function (window) {
             }
             
             // TODO 6 : YOUR CODE STARTS HERE //////////////////////
-            
+            if(circle.y > canvas.width){
+                circle.y = 0;
+            }
 
 
             // YOUR TODO 6 CODE ENDS HERE //////////////////////////
